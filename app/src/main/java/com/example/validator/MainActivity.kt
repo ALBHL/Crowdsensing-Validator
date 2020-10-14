b4105131709f03f12b9b13207208f4d22f06712a
ALBHL <allisonliu1999@gmail.com>
2020-10-14 02:25:00 -0400

    Add Authentication to Firebase

--- app/src/main/java/com/example/validator/MainActivity.kt
+++ app/src/main/java/com/example/validator/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log.d
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         register_button.setOnClickListener {
-            val email = email_edittext_reg.text.toString()
-            val password = password_edittext_reg.text.toString()
-
-            d("MainActivity", "Email is: " + email)
-            d("MainActivity", "Password is: $password")
+            performRegister()
         }
 
         already_have_account_textView.setOnClickListener {
@@ -26,5 +24,30 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun performRegister() {
+        val email = email_edittext_reg.text.toString()
+        val password = password_edittext_reg.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter text in email/password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        d("MainActivity", "Email is: " + email)
+        d("MainActivity", "Password is: $password")
+
+        // Firebase Authentication to create a user with email and password
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                // else successful
+                d("Main", "Successfully create user with uid: ${ it.result?.user?.uid }")
+            }
+            .addOnFailureListener {
+                d("Main", "Fail to create user: ${it.message}")
+            }
     }
 }
