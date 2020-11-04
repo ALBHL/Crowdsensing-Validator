aedeedefc1408792bf776f9db73246f727096dc4
ALBHL <allisonliu1999@gmail.com>
2020-11-03 19:36:41 -0500

    basic sqlite db

--- app/src/main/java/com/example/validator/ViewqueryActivity.kt
+++ app/src/main/java/com/example/validator/ViewqueryActivity.kt
@@ -8,34 +8,40 @@ import kotlinx.android.synthetic.main.outbox_file_row.*
 class ViewqueryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        title = "KotlinApp"
+        setContentView(R.layout.activity_viewquery)
+
         val context = this
         val db = DataBaseHandler(context)
+
         btnInsert.setOnClickListener {
             if (editTextName.text.toString().isNotEmpty() &&
-                editTextAge.text.toString().isNotEmpty()
-            ) {
+                editTextAge.text.toString().isNotEmpty()) {
                 val user = User(editTextName.text.toString(), editTextAge.text.toString().toInt())
                 db.insertData(user)
-                clearField()
-            }
-            else {
-                Toast.makeText(context, "Please Fill All Data's", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, "Please fill all datas", Toast.LENGTH_SHORT).show()
             }
         }
-        btnRead.setOnClickListener {
+
+
+        btn_read.setOnClickListener {
             val data = db.readData()
             tvResult.text = ""
             for (i in 0 until data.size) {
-                tvResult.append(
-                    data[i].id.toString() + " " + data[i].name + " " + data[i].age + "\n"
-                )
+                tvResult.append(data[i].id.toString() + " " + data[i].name + " " + data[i].age + "\n")
             }
         }
-    }
-    private fun clearField() {
-        editTextName.text.clear()
-        editTextAge.text.clear()
+
+
+        btn_update.setOnClickListener {
+            db.updateData()
+            btn_read.performClick()
+        }
+
+
+        btn_delete.setOnClickListener {
+            db.deleteData()
+            btn_read.performClick()
+        }
     }
 }
