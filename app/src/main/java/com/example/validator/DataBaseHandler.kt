477e7f76c5150816834d434c76424a155b84bc52
ALBHL <allisonliu1999@gmail.com>
2020-12-23 00:17:52 -0500

    update db stage to 1 field

--- app/src/main/java/com/example/validator/DataBaseHandler.kt
+++ app/src/main/java/com/example/validator/DataBaseHandler.kt
@@ -17,16 +17,14 @@ val COL_AGE = "age"
 val COL_ID = "id"
 val COL_URL = "imageurl"
 val COL_PROFILE = "profileimg"
-val COL_COLLECT = "is_collect" // true indicate collected, false indicate waiting to be collected
-val COL_READY = "is_ready"  // true means can be sent to be collected
-val COL_VALIDATE = "is_validate"
+//val COL_COLLECT = "is_collect" // true indicate collected, false indicate waiting to be collected
+val COL_STAGE = "current_stage"  // true means can be sent to be collected
+//val COL_VALIDATE = "is_validate"
 
 val createTable = "CREATE TABLE " + TABLE_NAME + " (" +
         COL_ID +" INTEGER PRIMARY KEY AUTOINCREMENT," +
         COL_NAME + " VARCHAR(256)," +
-        COL_COLLECT + " VARCHAR(256)," +
-        COL_READY + " VARCHAR(256)," +
-        COL_VALIDATE + " VARCHAR(256)," +
+        COL_STAGE + " VARCHAR(256)," +
         COL_AGE + " INTEGER," +
         COL_PROFILE + " VARCHAR(256)," +
         COL_URL + " VARCHAR(256))"
@@ -36,20 +34,19 @@ val dropTable = "DROP TABLE IF EXISTS " + TABLE_NAME
 val createTableIn = "CREATE TABLE " + TABLEIN_NAME + " (" +
         COL_ID +" INTEGER PRIMARY KEY AUTOINCREMENT," +
         COL_NAME + " VARCHAR(256)," +
-        COL_COLLECT + " VARCHAR(256)," +
-        COL_READY + " VARCHAR(256)," +
-        COL_VALIDATE + " VARCHAR(256)," +
+        COL_STAGE + " VARCHAR(256)," +
         COL_AGE + " INTEGER," +
         COL_PROFILE + " VARCHAR(256)," +
         COL_URL + " VARCHAR(256))"
 
 val dropTableIn = "DROP TABLE IF EXISTS " + TABLEIN_NAME
 
-class DataBaseHandler(var context: Context) : SQLiteOpenHelper(context,DATABASE_NAME,null,6) {
+class DataBaseHandler(var context: Context) : SQLiteOpenHelper(context,DATABASE_NAME,null,7) {
     override fun onCreate(db: SQLiteDatabase?) {
         db?.execSQL(createTableIn)
         db?.execSQL(createTable)
     }
+
 
     override fun onUpgrade(db: SQLiteDatabase?,oldVersion: Int,newVersion: Int) {
         db?.execSQL(dropTable)
@@ -57,38 +54,20 @@ class DataBaseHandler(var context: Context) : SQLiteOpenHelper(context,DATABASE_
         onCreate(db)
     }
 
+
     override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         onUpgrade(db, oldVersion, newVersion)
     }
 
-    fun insertData(user : User){ // add row into the outbox row
+
+    fun insertData(user : User){  // add row into the inbox rows
         val db = this.writableDatabase
         val cv = ContentValues()
         cv.put(COL_NAME, user.name)
         cv.put(COL_AGE, user.age)
         cv.put(COL_URL, user.imageurl)
         cv.put(COL_PROFILE, user.profileurl)
-        cv.put(COL_COLLECT, "true")
-        cv.put(COL_READY, "true")
-        cv.put(COL_VALIDATE, "true")
-        val result = db.insert(TABLE_NAME,null,cv)
-        if(result == -1.toLong())
-            Toast.makeText(context,"Failed",Toast.LENGTH_SHORT).show()
-        else
-            Toast.makeText(context,"Success",Toast.LENGTH_SHORT).show()
-    }
-
-
-    fun insertInData(user : User){  // add row into the inbox rows
-        val db = this.writableDatabase
-        val cv = ContentValues()
-        cv.put(COL_NAME, user.name)
-        cv.put(COL_AGE, user.age)
-        cv.put(COL_URL, user.imageurl)
-        cv.put(COL_PROFILE, user.profileurl)
-        cv.put(COL_COLLECT, "true")
-        cv.put(COL_READY, "true")
-        cv.put(COL_VALIDATE, "false")
+        cv.put(COL_STAGE,  user.cur_stage)
         val result = db.insert(TABLEIN_NAME,null,cv)
         if(result == -1.toLong())
             Toast.makeText(context,"Failed",Toast.LENGTH_SHORT).show()
@@ -96,34 +75,8 @@ class DataBaseHandler(var context: Context) : SQLiteOpenHelper(context,DATABASE_
             Toast.makeText(context,"Success",Toast.LENGTH_SHORT).show()
     }
 
+
     fun readData() : MutableList<User>{
-        val list : MutableList<User> = ArrayList()
-
-        val db = this.readableDatabase
-        val query = "Select * from " + TABLE_NAME
-        val result = db.rawQuery(query,null)
-        if(result.moveToFirst()){
-            do {
-                val user = User()
-                user.id = result.getString(result.getColumnIndex(COL_ID)).toInt()
-                user.name = result.getString(result.getColumnIndex(COL_NAME))
-                user.age = result.getString(result.getColumnIndex(COL_AGE)).toInt()
-                user.imageurl = result.getString(result.getColumnIndex(COL_URL))
-                user.ready = result.getString(result.getColumnIndex(COL_READY))
-                user.collect = result.getString(result.getColumnIndex(COL_COLLECT))
-                user.validate = result.getString(result.getColumnIndex(COL_VALIDATE))
-                user.profileurl = result.getString(result.getColumnIndex(COL_PROFILE))
-                list.add(user)
-            }while (result.moveToNext())
-        }
-
-        result.close()
-        db.close()
-        return list
-    }
-
-
-    fun readInData() : MutableList<User>{
         val list : MutableList<User> = ArrayList()
 
         val db = this.readableDatabase
@@ -136,9 +89,7 @@ class DataBaseHandler(var context: Context) : SQLiteOpenHelper(context,DATABASE_
                 user.name = result.getString(result.getColumnIndex(COL_NAME))
                 user.age = result.getString(result.getColumnIndex(COL_AGE)).toInt()
                 user.imageurl = result.getString(result.getColumnIndex(COL_URL))
-                user.ready = result.getString(result.getColumnIndex(COL_READY))
-                user.collect = result.getString(result.getColumnIndex(COL_COLLECT))
-                user.validate = result.getString(result.getColumnIndex(COL_VALIDATE))
+                user.cur_stage = result.getString(result.getColumnIndex(COL_STAGE))
                 user.profileurl = result.getString(result.getColumnIndex(COL_PROFILE))
                 list.add(user)
             }while (result.moveToNext())
@@ -150,14 +101,6 @@ class DataBaseHandler(var context: Context) : SQLiteOpenHelper(context,DATABASE_
     }
 
 
-    fun deleteData(){
-        val db = this.writableDatabase
-        db.delete(TABLE_NAME,null,null)
-        db.delete(TABLEIN_NAME,null,null)
-        db.close()
-    }
-
-
     fun updateData() {
         val db = this.writableDatabase
         db?.execSQL(dropTable)
@@ -165,16 +108,17 @@ class DataBaseHandler(var context: Context) : SQLiteOpenHelper(context,DATABASE_
         onCreate(db)
     }
 
-    fun deleteInRow(name: String) {
+    fun deleteInRow(id: String) {
         val db = this.writableDatabase
-        db.delete(TABLEIN_NAME, "$COL_NAME=?", arrayOf(name))
+        db.delete(TABLEIN_NAME, "$COL_ID=?", arrayOf(id))
     }
 
-    fun updateInRow(name: String) {
+
+    fun updateRow(id: String, stage: String) {
         val db = this.writableDatabase
         val cv = ContentValues()
-        cv.put(COL_VALIDATE, "true")
-        db.update(TABLEIN_NAME, cv,"$COL_NAME=?", arrayOf(name))
+        cv.put(COL_STAGE, stage)/// TODO: something wrong here
+        db.update(TABLEIN_NAME, cv,"$COL_ID=?", arrayOf(id))
     }
 
 }
--- app/src/main/java/com/example/validator/ValidateImageActivity.kt
+++ app/src/main/java/com/example/validator/ValidateImageActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log.d
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_show_image.*
@@ -20,34 +21,30 @@ class ValidateImageActivity : AppCompatActivity() {
         val cur_name = intent.getStringExtra(InboxActivity.ROW_NAME)
         val context = this
         val db = DataBaseHandler(context)
-        val data = db.readInData()
+        val data = db.readData()
 
         textview_metadata.text = ""
         textview_metadata.text = "Inbox\n"
         for (i in 0 until data.size) {
             textview_metadata.append(data[i].id.toString() + " " + data[i].name + " " + data[i].age + data[i].imageurl +
-                    "READY: " + data[i].ready + "COLLECT: " + data[i].collect + "VALIDATE: " + data[i].validate + "\n")
+                    "STAGE: " + data[i].cur_stage + "\n")
         }
 
         recyclerViewContents.layoutManager = LinearLayoutManager(this)
         recyclerViewContents.adapter = images?.let { ValidateImageAdapter(it) }
 
         buttonDisapprove.setOnClickListener {
-            if (cur_name != null) {
-                db.updateInRow(cur_name)
+            if (cur_id != null) {
+                db.updateRow(cur_id, "deleted")
             }
             val intent = Intent(this, SuccessValidateActivity::class.java)
             startActivity(intent)
         }
 
         ButtonValidation.setOnClickListener {
-            var user = User()
-            if (cur_name != null && images != null) {
-                user = User(cur_name, 1, images)
-                db.insertData(user)
-            }
-            if (cur_name != null) {
-                db.deleteInRow(cur_name)
+            Toast.makeText(context,cur_id, Toast.LENGTH_SHORT).show()
+            if (cur_id != null) {
+                db.updateRow(cur_id, "validated")
             }
             val intent = Intent(this, SuccessValidateActivity::class.java)
             startActivity(intent)
